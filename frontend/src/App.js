88149d848c208f471e0dd6e4a65f6ed648db177a
Yu Liu <sharonliuyu@gmail.com>
2021-01-29 20:07:08 -0800

    React set up

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//jss, use classname as attribute, can have javascript inside
+// function App() {
+  //arrow function works the same
+const App = () => {
+  return (
+    // the return has to be one-element
+    <>
+      <h1> welcome to proshop</h1>
+    </>
+  );
+}
+
+export default App;
